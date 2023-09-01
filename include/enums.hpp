2a0e28b4614e5c859f620419d602be043aa1bec7
John Mastroberti <johnmastroberti123@gmail.com>
2023-09-01 16:11:15 -0400

    converting Shift to string

--- include/enums.hpp
+++ include/enums.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <string>
 
 enum class SkillLevel : std::uint8_t {
   None,
@@ -20,3 +21,31 @@ enum class Shift : std::uint8_t {
   FridayMorning,
   FridayAfternoon,
 };
+
+inline std::string to_string(Shift s) {
+  using enum Shift;
+  switch (s) {
+    case MondayMorning:
+      return "Monday Morning";
+    case MondayAfternoon:
+      return "Monday Afternoon";
+    case TuesdayMorning:
+      return "Tuesday Morning";
+    case TuesdayAfternoon:
+      return "Tuesday Afternoon";
+    case WednesdayMorning:
+      return "Wednesday Morning";
+    case WednesdayAfternoon:
+      return "Wednesday Afternoon";
+    case ThursdayMorning:
+      return "Thursday Morning";
+    case ThursdayAfternoon:
+      return "Thursday Afternoon";
+    case FridayMorning:
+      return "Friday Morning";
+    case FridayAfternoon:
+      return "Friday Afternoon";
+    default:
+      return "";
+  }
+}
